aaf0be1f2f63b660b1e8e6dfbab0d3f2d9558adf
Rod Conan <rconan@gmto.org>
2021-05-20 17:15:01 -0700

    UPDATE DOS API EXAMPLE

--- examples/ch01-02-dos-api/src/main.rs
+++ examples/ch01-02-dos-api/src/main.rs
@@ -40,6 +40,7 @@ impl Dos for Integrate {
     type Output = Vec<f64>;
     fn inputs(&mut self, data: Option<Vec<IO<Self::Input>>>) -> Result<&mut Self, DOSIOSError> {
         self.u = data
+            // Identifying M2 42 segment rigid body motion IO variant
             .map(|data| {
                 data.into_iter().find_map(|io| match io {
                     IO::MCM2RB6D { data: value } => value,
@@ -47,26 +48,25 @@ impl Dos for Integrate {
                 })
             })
             .flatten()
+            // Extracting M2 14 Rx and Ry rigid body motions
             .map(|x| {
                 x.chunks(6)
                     .flat_map(|x| x[3..5].to_vec())
                     .collect::<Vec<f64>>()
             })
+            // Throwing an error if IO::MCM2RB6D is missing
             .ok_or_else(|| DOSIOSError::Inputs("Integrate MCM2RB6D IO input error".into()))?;
         Ok(self)
     }
     fn outputs(&mut self) -> Option<Vec<IO<Self::Output>>> {
         self.output()
+            // Assigning M2 segment Rx and Ry to rigid body motion vector
             .map(|y| {
                 y.chunks(2)
-                    .flat_map(|y| {
-                        let mut data = vec![0f64; 6];
-                        data[3] = y[0];
-                        data[4] = y[1];
-                        data
-                    })
+                    .flat_map(|y| vec![0f64, 0f64, 0f64, y[0], y[1], 0f64])
                     .collect::<Vec<f64>>()
             })
+            // Embedding the rigid body motions into the corresponding IO variant
             .map(|x| vec![jar::MCM2RB6D::io_with(x)])
     }
 }
